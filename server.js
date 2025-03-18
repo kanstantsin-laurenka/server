28fb32352713829365d5df49bc0c51b51ecd44b4
Kanstantsin Laurenka <132393513+koslavr@users.noreply.github.com>
2025-03-18 17:52:26 +0400

    Change code

--- server.js
+++ server.js
@@ -2,20 +2,59 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+
+async function getInstanceMetadata() {
+  try {
+    const tokenResponse = await axios.put('http://169.254.169.254/latest/api/token', null, {
+      headers: { 'X-aws-ec2-metadata-token-ttl-seconds': '21600' }
+    });
+    const token = tokenResponse.data;
+    
+    const regionResponse = await axios.get('http://169.254.169.254/latest/meta-data/placement/region', {
+      headers: { 'X-aws-ec2-metadata-token': token }
+    });
+    const azResponse = await axios.get('http://169.254.169.254/latest/meta-data/placement/availability-zone', {
+      headers: { 'X-aws-ec2-metadata-token': token }
+    });
+    
+    return {
+      region: regionResponse.data,
+      availabilityZone: azResponse.data
+    };
+  } catch (error) {
+    console.error('Error fetching instance metadata:', error);
+    return null;
+  }
+}
+
 
 app.get('/', async (req, res) => {
   try {
-    const response = await axios.get('http://169.254.169.254/latest/meta-data/placement/availability-zone');
-    const az = response.data;
-    const region = az.slice(0, -1);
+    res.send('Hello World');
     
-    res.json({ region, availabilityZone: az });
+    // const response = await axios.get('http://169.254.169.254/latest/meta-data/placement/availability-zone');
+    // const az = response.data;
+    // const region = az.slice(0, -1);
+    
+    // res.json({ region, availabilityZone: az });
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch metadata" });
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get('/metadata', async (req, res) => {
+  try {
+    const metadata = await getInstanceMetadata();
+    if (metadata) {
+      res.json(metadata);
+    } else {
+      res.status(500).json({ error: 'Failed to retrieve instance metadata' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch metadata" });
+  }
+});
+
+app.listen(80, () => {
+  console.log(`Server is running on port 80`);
 });
