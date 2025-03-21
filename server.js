e7218cdf30431bbc31926fe9047d31545bc0558f
Kanstantsin Laurenka <132393513+koslavr@users.noreply.github.com>
2025-03-21 20:34:10 +0400

    Create images application

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 const imageRoutes = require('./routes/images');
 app.use('/', imageRoutes);
 
+app.get('/', (req, res) => {
+  res.send('<html><body><h1>Welcome to the Web App</h1></body></html>');
+});
+
 app.listen(80, () => {
   console.log('Server running on port 80');
 });
